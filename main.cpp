7395cd868827fb1ba44508c641062c0948df47be
mavioux <pakmarios@gmail.com>
2019-12-15 21:27:04 +0200

    finished development

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 static int Bsize =5;
 
-vector<double> myconvolution(vector<double> A, vector <double > B){
+vector<double> *myconvolution(vector<double> &A, vector <double > &B){
     int size = A.size()+B.size()-1;
-    vector<double> y(size,0.0);
+    vector<double> *y=new vector<double>(size,0.0);
     int kappa;
     int keepo;
     for(auto i(0); i < size; ++i) {
@@ -26,7 +26,7 @@ vector<double> myconvolution(vector<double> A, vector <double > B){
         }
         for (auto j(kappa); j <= keepo; ++j) {
             if (!(i-j<0)) {
-                y[i] += (A[j] * B[i - j]);
+                (*y)[i] += (A[j] * B[i - j]);
             }
         }
     }
@@ -39,23 +39,19 @@ int main() {
     cout << "Ergasia Ylopoihshs sunelhkshs " << endl;
     //Xrhsimopoiontas thn bibliothhkh pairnoume ta  arxeia wav kai dhmiourgoume 2 vector me ta samples tous
     AudioFile<double> pinkNoise;
-    pinkNoise.load (R"(C:\Users\MAXIM\CLionProjects\untitled\pink_noise.wav)");
+    pinkNoise.load ("./pink_noise2.wav");
     AudioFile<double> sampleAudio;
-    sampleAudio.load (R"(C:\Users\MAXIM\CLionProjects\untitled\sample_audio.wav)");
-    vector<double> audiosample;
-    for (long i = 0; i <sampleAudio.getNumSamplesPerChannel() ;i++)
-    {
-        audiosample.push_back(sampleAudio.samples[0][i]);
-    }
+    sampleAudio.load ("./sample_audio1.wav");
+    vector<double> audiosample = sampleAudio.samples[0];
     //vector audiosample periexei ta samples tou arxeiou hxou
-    vector<double> noise;
-    for (long i = 0; i < pinkNoise.getNumSamplesPerChannel(); i++)
-    {
-        noise.push_back(pinkNoise.samples[0][i]);
-    }
+    vector<double> noise= pinkNoise.samples[0];
     //vector noise periexei ta samples tou thorubou
     cout<<"egine h eisagwgh stous vector "<<endl;
     vector<double> Y2;
+    AudioFile<double> convolution1;
+    AudioFile<double> convolution2;
+    
+    convolution1.setSampleRate(sampleAudio.getSampleRate());
     //vector me to convolution tou 2ou erwthmatos me ta audiofiles
 
     //Meros A1 me ta shmata A kai B
@@ -76,17 +72,20 @@ int main() {
     }
 
     //prwth sunelhksh me A kai B
-    Y1=myconvolution(A,B);
+    Y1=*myconvolution(A,B);
     cout<<"apotelesma tou A1:"<<endl;
     for(int i =0; i<Y1.size();i++){
         cout <<Y1[i]<<endl;
     }
 
     //deuterh sunelhksh me sample kai noise
-    Y2 = myconvolution(audiosample,noise);
-    for(int i =0; i<Y2.size();i++){
-        cout <<Y2[i]<<endl;
+    convolution1.samples[0] =*myconvolution(audiosample,noise);
+    convolution1.save("./pinkNoise_sampleAudio.wav");
+    for (int i=0;i<pinkNoise.samples[0].size();i++){
+        pinkNoise.samples[0][i]=static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
     }
+    convolution2.samples[0]=*myconvolution(audiosample,noise);
+    convolution2.save("./whiteNoise_sampleAudio.wav");
     return 0;
 }
 
